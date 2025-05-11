51caf88e5c6cdc2e42e7bd8f5a672b6ae1c3acb3
Aditya Tuljapurkar <adityatuljapurkar90018@gmail.com>
2025-05-11 23:58:08 +0530

    creation of Profice {parent} and Avatar{child}

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import { Profiler, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.jsx'
+
+import Profile from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+<Profile/>    
   </StrictMode>,
 )
