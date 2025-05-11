14368c25578228d086e6cc64ae2272af67ffc44a
Aditya Tuljapurkar <adityatuljapurkar90018@gmail.com>
2025-05-12 01:49:23 +0530

    used propsfor the communication between the components

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,40 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import image1 from './assets/resourses/1.jpg'
-
-function Avatar() {
+import {getImageUrl} from './assets/util.js'
+function Avatar({person , size}) {
 
   return (
     <>
   <img
   className='avatar' 
-  src={image1} 
-  alt="img1"
-  width = {100}
+  src={getImageUrl(person)} 
+  alt={person.name}
+  width = {size}
   height = {100}
    />
-
+  <br />
     </>
   )
 }
 
 export default function Profile(){
   return (
-  <Avatar/>
+    <div>
+  <Avatar
+   person = {{ name : "Bobby" , imageId : "2"  }}
+   size = {100}
+  />
+  
+  <Avatar 
+  person={{name : "Chagan" , imageId : "7"}}
+size = {100}  
+  />
+  <Avatar
+  person={{name : 'Topu Ustad', imageId : "4"}}
+  size = {100}
+  />
+  </div>
+  
   )
 }
---
+++ src/assets/util.js
@@ -0,0 +1,8 @@
+export function getImageUrl(person,size = 's'){
+return(
+    'https://randomuser.me/api/portraits/lego/'+
+    person.imageId +'.jpg'
+    
+    // https://randomuser.me/api/portraits/lego/1.jpg
+)
+}
